8385152edb4521fa95a9d90cba72c4d85b35f48b
Dustin <dustidusk@gmail.com>
2022-03-07 23:56:11 +0700

    Alter layout, finish dashboard layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        layout: '1fr 2fr 2fr 2fr',
+      },
+      gridTemplateRows: {
+        dashboard: '2fr 4fr 3fr 4fr',
+        layout: '2fr 3fr 3fr 3fr',
+      },
+      fontFamily: {
+        dashboard: "'Raleway', sans-serif;",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require('tailwind-scrollbar')],
+};
