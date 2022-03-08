1427326b58ed22ad3686bee7bb961a961eac18d5
Dustin <dustidusk@gmail.com>
2022-03-08 17:46:40 +0700

    Add trending, announcement layout and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,14 @@ module.exports = {
       gridTemplateColumns: {
         layout: '1fr 2fr 2fr 2fr',
         header: '3fr 2fr',
-        projects: 'repeat(auto-fit, minmax(300px, 1fr))',
+        projects: 'repeat(auto-fit, minmax(400px, 1fr))',
       },
       gridTemplateRows: {
         dashboard: '2fr 4fr 3fr 4fr',
         layout: '2fr 3fr 3fr 3fr',
         header: '1fr 1fr',
         projects: 'repeat(auto-fit, minmax(400px, 1fr))',
+        'project-card': '1fr 3fr 1fr',
       },
       fontFamily: {
         whole: "'Raleway', sans-serif;",
